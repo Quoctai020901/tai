94a4df033dda685b6e496c75cbcfde83aa7668e1
Quoctai020901 <99971842+Quoctai020901@users.noreply.github.com>
2022-04-21 10:02:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ red='\033[0;31m'
 green='\033[0;32m'
 #yellow='\033[0;33m'
 plain='\033[0m'
-operation=(Install stop start Update UpdateConfig logs restart delete)
+operation=(Install UpdateConfig caidat(4ghatde) )
 # Make sure only root can run our script
 [[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] Chưa vào root kìa !, vui lòng xin phép ROOT trước!" && exit 1
 
@@ -104,7 +104,46 @@ error_detect_depends() {
 }
 
 # Pre-installation settings
-pre_install_xrayr() {
+pree_install_xrayr() {
+  #link web:
+read -p "link web(bao gồm https://): " ApiHost
+  [ -z "${ApiHost}" ] && ApiHost="0"
+  echo "-------------------------------"
+  echo "Link web: ${ApiHost}"
+  echo "-------------------------------"
+  
+  #key web:
+ read -p "key web: " ApiKey
+  [ -z "${ApiKey}" ] && ApiKey="0"
+  echo "-------------------------------"
+  echo "key web: ${ApiKey}"
+  echo "-------------------------------"
+
+  
+  read -p " ID nút (Node_ID):" node_id
+  [ -z "${node_id}" ] && node_id=0
+  echo "-------------------------------"
+  echo -e "Node_ID: ${node_id}"
+  echo "-------------------------------"
+  
+
+  #giới hạn thiết bị
+read -p "Giới hạn thiết bị :" DeviceLimit
+  [ -z "${DeviceLimit}" ] && DeviceLimit="0"
+  echo "-------------------------------"
+  echo "Thiết bị tối đa là: ${DeviceLimit}"
+  echo "-------------------------------"
+  
+   #IP vps
+read -p "Nhập domain hoặc ip  :" CertDomain
+  [ -z "${CertDomain}" ] 
+  echo "-------------------------------"
+  echo "ip : ${CertDomain}"
+  echo "-------------------------------"
+  
+  
+}
+pree_install_xrayr() {
   #link web:
 #read -p "link web(bao gồm https://): " ApiHost
  # [ -z "${ApiHost}" ] && ApiHost="0"
@@ -147,7 +186,7 @@ read -p "Nhập domain hoặc ip  :" CertDomain
 
 
 # Config docker
-config_xrayr() {
+config_xrayrr() {
   cd ${cur_dir} || exit
   cd /etc/XrayR
   cat >config.yml <<EOF
@@ -214,27 +253,74 @@ EOF
   sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
 
   }
+  # Config docker
+config_xrayr() {
+  cd ${cur_dir} || exit
+  cd /etc/XrayR
+  cat >config.yml <<EOF
+Log:
+  Level: none # Log level: none, error, warning, info, debug 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
+ConnetionConfig:
+  Handshake: 4 # Handshake time limit, Second
+  ConnIdle: 10 # Connection idle time limit, Second
+  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 64 # The internal cache size of each connection, kB 
+Nodes:
+  -
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    ApiConfig:
+      ApiHost: "https://4ghatde.me"
+      ApiKey: "phamvanquoctai0209"
+      NodeID: 41
+      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      DisableUploadTraffic: false # Disable Upload Traffic to the panel
+      DisableGetRule: false # Disable Get Rule from the panel
+      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
+      DisableSniffing: true # Disable domain sniffing 
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "node1.test.com" # Domain to cert
+        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/cert/node1.test.com.key
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          ALICLOUD_ACCESS_KEY: aaa
+          ALICLOUD_SECRET_KEY: bbb
+EOF
+   sed -i "s|ApiHost:.*|ApiHost: \"${ApiHost}\"|" ./config.yml
+  sed -i "s|ApiKey:.*|ApiKey: \"${ApiKey}\"|" ./config.yml
+  sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./config.yml
+  sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
+  sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
 
-
-
-
-
-
-#update_image
-Update_xrayr() {
-  cd ${cur_dir}
-  echo "Tải hình ảnh DOCKER"
-  docker-compose pull
-  echo "Bắt đầu chạy dịch vụ DOCKER"
-  docker-compose up -d
-}
-
-#show last 100 line log
-
-logs_xrayr() {
-  echo "100 dòng nhật ký chạy sẽ được hiển thị"
-  xrayr log 100
-}
+  }
 
 # Update config
 UpdateConfig_xrayr() {
@@ -246,33 +332,20 @@ UpdateConfig_xrayr() {
   xrayr start
 }
 
-restart_xrayr() {
-  cd ${cur_dir}
-  docker-compose down
-  docker-compose up -d
-  echo "Khởi động lại thành công!"
-}
-delete_xrayr() {
-  xrayr uninstall
-}
-#Stop
-stop_xrayr() {
-  cd ${cur_dir}
-  xrayr top
-  echo "Đã dừng!"
-}
-  start_xrayr() {
-  cd ${cur_dir}
-  xrayr start
- 
-  }
-
 
 
 # Install xrayr
 Install_xrayr() {
   bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/XrayR-release/main/install.sh)
 
+}
+caidat_xrayr() {
+  bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/XrayR-release/main/install.sh)
+ pree_install_xrayr
+  config_xrayrr
+  cd /root
+  echo "Bắt đầu chạy dịch vụ "
+  xrayr start
 }
 
 # Initialization step
@@ -288,7 +361,7 @@ while true; do
   read -p "Vui lòng chọn một số và nhấn Enter (Enter theo mặc định ${operation[0]}):" selected
   [ -z "${selected}" ] && selected="1"
   case "${selected}" in
-  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8)
+  1 | 2 | 3 )
     echo
     echo "Bắt Đầu : ${operation[${selected} - 1]}"
     echo
@@ -296,7 +369,7 @@ while true; do
     break
     ;;
   *)
-    echo -e "[${red}Error${plain}] Vui lòng nhập số chính xác [1-8]"
+    echo -e "[${red}Error${plain}] Vui lòng nhập số chính xác"
     ;;
   esac
 done

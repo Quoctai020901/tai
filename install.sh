0e6c697e465d898f2f54e0d390ab3792efe0e1fb
Quoctai020901 <99971842+Quoctai020901@users.noreply.github.com>
2022-04-30 20:05:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ Nodes:
   -
     PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
     ApiConfig:
-      ApiHost: "https://4g.quoctai.xyz"
+      ApiHost: "https://4ghatde.com"
       ApiKey: "phamvanquoctai0209"
       NodeID: 41
       NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin

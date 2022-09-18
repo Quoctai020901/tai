75e1ceee1a4171ea9ab2a15704621068d4e3235e
Quoctai020901 <99971842+Quoctai020901@users.noreply.github.com>
2022-09-18 21:39:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,30 @@ red='\033[0;31m'
 green='\033[0;32m'
 #yellow='\033[0;33m'
 plain='\033[0m'
-operation=(hatde giare sieure update_hatde update_giare update_sieure )
+operation=(install update )
 # Make sure only root can run our script
 [[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] Chưa vào root kìa !, vui lòng xin phép ROOT trước!" && exit 1
 
 
 # Pre-installation settings
 pre_install() {
- #link web:
-#read -p "link web(bao gồm https://): " ApiHost
- # [ -z "${ApiHost}" ] && ApiHost="0"
- # echo "-------------------------------"
- # echo "Link web: ${ApiHost}"
- # echo "-------------------------------"
+ echo -e "[1] 4ghatde.com"
+  echo -e "[2] 4g.giare.me"
+  echo -e "[3] 4gsieure.net"
+  read -p "Web đang sử dụng:" api_host
+  if [ "$api_host" == "1" ]; then
+    api_host="https://4ghatde.com"
+  elif [ "$api_host" == "2" ]; then
+    api_host="https://4g.giare.me"
+    elif [ "$api_host" == "3" ]; then
+    api_host="https://4gsieure.net"
+  else 
+    api_host="https://4ghatde.com"
+  fi
+
+  echo "--------------------------------"
+  echo "Bạn đã chọn ${api_host}"
+  echo "--------------------------------"
   
   #key web:
 # read -p "key web: " ApiKey
@@ -63,79 +74,8 @@ read -p "Giới hạn tốc độ :" SpeedLimit
   
 }
 
-
-
-# Config giare
-config_giare() {
- # cd ${cur_dir} || exit
-  cd /etc/XrayR
-  cat >config.yml <<EOF
-Log:
-  Level: none # Log level: none, error, warning, info, debug 
-  AccessPath: # /etc/XrayR/access.Log
-  ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
-ConnetionConfig:
-  Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 10 # Connection idle time limit, Second
-  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
-  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
-Nodes:
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
-    ApiConfig:
-      ApiHost: "https://4g.giare.me"
-      ApiKey: "phamvanquoctai0209"
-      NodeID: 41
-      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
-      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
-    ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: True # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-      CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: test@me.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          ALICLOUD_ACCESS_KEY: aaa
-          ALICLOUD_SECRET_KEY: bbb
-EOF
-  # sed -i "s|ApiHost:.*|ApiHost: \"${ApiHost}\"|" ./config.yml
- # sed -i "s|ApiKey:.*|ApiKey: \"${ApiKey}\"|" ./config.yml
-  sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./config.yml
-  sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
-  sed -i "s|SpeedLimit:.*|SpeedLimit: ${SpeedLimit}|" ./config.yml
- # sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
-
-  }
-  # Config hatde
-config_hatde() {
+ # Config 
+config_xrayr() {
   # cd ${cur_dir} || exit
   cd /etc/XrayR
   cat >config.yml <<EOF
@@ -201,69 +141,6 @@ EOF
   sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
   sed -i "s|SpeedLimit:.*|SpeedLimit: ${SpeedLimit}|" ./config.yml
   sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
-
-  }
-# config web sieure
-config_sieure() {
- # cd ${cur_dir} || exit
-  cd /etc/XrayR
-  cat >config.yml <<EOF
-Log:
-  Level: none # Log level: none, error, warning, info, debug 
-  AccessPath: # /etc/XrayR/access.Log
-  ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
-ConnetionConfig:
-  Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 10 # Connection idle time limit, Second
-  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
-  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
-Nodes:
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
-    ApiConfig:
-      ApiHost: "https://sieurespeed.me"
-      ApiKey: "phamvanquoctai0209"
-      NodeID: 41
-      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
-      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
-    ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: True # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-      CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
-        CertFile: /etc/XrayR/4ghatde.crt # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/4ghatde.key
-        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: test@me.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          ALICLOUD_ACCESS_KEY: aaa
-          ALICLOUD_SECRET_KEY: bbb
-EOF
 cd /etc/XrayR
 cat >4ghatde.crt <<EOF
 -----BEGIN CERTIFICATE-----
@@ -322,85 +199,44 @@ iwgnzmzrH3LAoZleTtxKE+o=
 -----END PRIVATE KEY-----
 EOF
 
+  }
+
+
    #sed -i "s|ApiHost:.*|ApiHost: \"${ApiHost}\"|" ./config.yml
  # sed -i "s|ApiKey:.*|ApiKey: \"${ApiKey}\"|" ./config.yml
   sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./config.yml
   sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
   sed -i "s|SpeedLimit:.*|SpeedLimit: ${SpeedLimit}|" ./config.yml
-#  sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
+  sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
 
   }
 
 
-# Update config hatde
-update_hatde_xrayr() {
+# Update config 
+update_xrayr() {
   
   pre_install
-  config_hatde
-  cd /root
-  echo "Bắt đầu chạy dịch vụ "
-  
-  xrayr restart
-}
-
-
-
-
-# Update config giare
-update_giare_xrayr() {
-
-  pre_install
-  config_giare
-  cd /root
-  echo "Bắt đầu chạy dịch vụ "
-  
-  xrayr restart
-}
-# Update config web sieure
-update_sieure_xrayr() {
-
-  pre_install
-  config_sieure
+  config_xrayr
   cd /root
   echo "Bắt đầu chạy dịch vụ "
   
   xrayr restart
 }
 
-# Install xrayr web phu
-giare_xrayr() {
-  bash <(curl -Ls https://raw.githubusercontent.com/qtai2901/XrayR-release/main/install.sh)
-  clear
- pre_install
- config_giare
-  cd /root
-  echo "Bắt đầu chạy dịch vụ "
-  xrayr start
-}
 
-# Install xrayr hatde
-hatde_xrayr() {
+
+
+# Install xrayr 
+install_xrayr() {
   bash <(curl -Ls https://raw.githubusercontent.com/qtai2901/XrayR-release/main/install.sh)
   clear
   pre_install
-  config_hatde
+  config_xrayr
   cd /root
   echo "Bắt đầu chạy dịch vụ "
   xrayr start
 }
 
-
-
-# Install xrayr web sieure
-sieure_xrayr() {
-  bash <(curl -Ls https://raw.githubusercontent.com/qtai2901/XrayR-release/main/install.sh)
-  clear
- pre_install
-  config_sieure
-  cd /root
-  echo "Bắt đầu chạy dịch vụ "
-  xrayr start
-}
 
 
 # Initialization step
@@ -416,7 +252,7 @@ while true; do
   read -p "Vui lòng chọn một số và nhấn Enter (Enter theo mặc định ${operation[0]}):" selected
   [ -z "${selected}" ] && selected="1"
   case "${selected}" in
-  1 | 2 | 3 | 4 | 5 | 6 )
+  1 | 2 )
     echo
     echo "Bắt Đầu : ${operation[${selected} - 1]}"
     echo
